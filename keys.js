630c49029641c4824a9911eaafbb4fc29fb367a6
Andy Walpole <me@andywalpole.me>
2017-01-31 07:25:24 +0000

    added google speech api node library

---
+++ keys.js
@@ -0,0 +1,5 @@
+const keys = {
+  API: 'AIzaSyC7-rra7hvbi_LlC8OkYTVFCIqhrRgOffM'
+};
+
+module.exports = keys;
--- routes/post_audio.js
+++ routes/post_audio.js
@@ -5,9 +5,44 @@ const fs = require('fs');
 const fileType = require('file-type');
 const readChunk = require('read-chunk');
 
+// [START speech_quickstart]
+// Imports the Google Cloud client library
+const Speech = require('@google-cloud/speech');
+
+// Your Google Cloud Platform project ID
+const projectId = require('../keys.js');
+
+
+// [START speech_async_recognize]
+function asyncRecognize(filename) {
+  // Instantiates a client
+  const speech = Speech();
+
+  const config = {
+    // Configure these settings based on the audio you're transcribing
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+  };
+
+  // Detects speech in the audio file, e.g. "./resources/audio.raw"
+  // This creates a recognition job that you can wait for now, or get its result
+  // later.
+  return speech.startRecognition(filename, config)
+    .then((results) => {
+      const operation = results[0];
+      // Get a Promise represention the final result of the job
+      return operation.promise();
+    })
+    .then((transcription) => {
+      console.log(`Transcription: ${transcription}`);
+      return transcription;
+    });
+}
+// [END speech_async_recognize]
+
 //function will check if a directory exists, and create it if it doesn't
 function checkDirectory(directory, callback) {
-  fs.stat(directory, function(err, stats) {
+  fs.stat(directory, function (err) {
     //Check if error defined and the error code is "not exists"
     if (err && err.errno === 34) {
       //Create the directory, call the callback.
@@ -19,13 +54,13 @@ function checkDirectory(directory, callback) {
   });
 }
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 
   req.pipe(req.busboy);
 
-  req.busboy.on('file', function(fieldname, file, filename) {
+  req.busboy.on('file', function (fieldname, file, filename) {
 
-    checkDirectory(__dirname + '/files/', function(err) {
+    checkDirectory(__dirname + '/files/', function (err) {
 
       if (err) {
         return err;
@@ -35,27 +70,13 @@ router.post('/', function(req, res) {
 
         file.pipe(fstream);
 
-        fstream.on('close', function() {
+        fstream.on('close', function () {
 
           const buffer = readChunk.sync(__dirname + '/files/' + filename, 0, 4100);
           const fileExt = fileType(buffer);
 
-          const opts = {
-            file: __dirname + '/files/' + filename,
-            key: 'AIzaSyA0_hF7fdP2iKlNYhCVfPvApchZmxYBqD8',
-            filetype: fileExt.ext
-          };
-
-          speech(opts, function(err, results) {
-
-            if (err) {
-              console.dir(err);
-              return err;
-            }
-
-            console.log(results);
-            res.send('Post page');
-            // [{result: [{alternative: [{transcript: '...'}]}]}]
+          asyncRecognize(__dirname + '/files/' + filename).then(function (returnedData) {
+            res.send(JSON.stringify(returnedData));
           });
 
         });
