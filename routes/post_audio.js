1686785111e7838c4f790ff44a7d0addaa86dc0e
Andy Walpole <me@andywalpole.me>
2017-01-31 12:57:02 +0000

    added Aurora js library

--- routes/post_audio.js
+++ routes/post_audio.js
@@ -4,19 +4,45 @@ const speech = require('google-speech-api');
 const fs = require('fs');
 const fileType = require('file-type');
 const readChunk = require('read-chunk');
+const AV = require('av');
 
 // [START speech_quickstart]
 // Imports the Google Cloud client library
-const Speech = require('@google-cloud/speech');
 
 // Your Google Cloud Platform project ID
 const projectId = require('../keys.js');
 
+const Speech = require('@google-cloud/speech')({
+  projectId: projectId,
+  // Or the contents of the key file:
+  credentials: require('../MyProject-ff36826dd0b9.json')
+});
+
+// [START speech_sync_recognize]
+function syncRecognize(filename) {
+  // Instantiates a client
+
+  const config = {
+    // Configure these settings based on the audio you're transcribing
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+  };
+
+  // Detects speech in the audio file, e.g. "./resources/audio.raw"
+  return Speech.recognize(filename, config)
+    .then((results) => {
+      const transcription = results[0];
+      console.log(`Transcription: ${transcription}`);
+      return transcription;
+    }).catch((error) => {
+      console.log(`Error: ${error}`);
+    });
+}
+// [END speech_sync_recognize]
+
 
 // [START speech_async_recognize]
 function asyncRecognize(filename) {
-  // Instantiates a client
-  const speech = Speech();
 
   const config = {
     // Configure these settings based on the audio you're transcribing
@@ -27,10 +53,9 @@ function asyncRecognize(filename) {
   // Detects speech in the audio file, e.g. "./resources/audio.raw"
   // This creates a recognition job that you can wait for now, or get its result
   // later.
-  return speech.startRecognition(filename, config)
+  return Speech.startRecognition(filename, config)
     .then((results) => {
       const operation = results[0];
-      console.dir(operation);
       // Get a Promise represention the final result of the job
       return operation.promise();
     })
@@ -46,7 +71,7 @@ function asyncRecognize(filename) {
 
 //function will check if a directory exists, and create it if it doesn't
 function checkDirectory(directory, callback) {
-  fs.stat(directory, function(err) {
+  fs.stat(directory, function (err) {
     //Check if error defined and the error code is "not exists"
     if (err && err.errno === 34) {
       //Create the directory, call the callback.
@@ -58,25 +83,34 @@ function checkDirectory(directory, callback) {
   });
 }
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 
   req.pipe(req.busboy);
 
-  req.busboy.on('file', function(fieldname, file, filename) {
+  req.busboy.on('file', function (fieldname, file, filename) {
 
     const fstream = fs.createWriteStream(__dirname + '/files/' + filename);
 
     file.pipe(fstream);
 
-    fstream.on('close', function() {
+    fstream.on('close', function () {
 
-      const buffer = readChunk.sync(__dirname + '/files/' + filename, 0, 4100);
+      console.dir(global.AudioContext);
+
+      const asset = AV.Asset.fromFile(__dirname + '/files/' + filename);
+      asset.get('duration', function(duration) {
+        console.dir(duration);
+        // do something
+        res.send(true);
+      });
+
+    /*  const buffer = readChunk.sync(__dirname + '/files/' + filename, 0, 4100);
       const fileExt = fileType(buffer);
 
-      asyncRecognize(__dirname + '/files/' + filename).then(function(returnedData) {
+      asyncRecognize(__dirname + '/files/' + filename).then(function (returnedData) {
         console.dir(returnedData);
         res.send(JSON.stringify(returnedData));
-      });
+      });*/
 
     });
 
